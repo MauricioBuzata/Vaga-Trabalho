f0c396c2dd88ef24772240a07a72bf7f8d6cbda4
unknown <mauricio_buzata@hotmail.com>
2017-05-29 12:26:32 -0300

    Adicionando segunda tarefa, script de busca do 10 funcionários

---
+++ etapa2.sql
@@ -0,0 +1,16 @@
+﻿SELECT dept_name AS Departamento, concat(e.first_name," ",e.last_name) AS NomeComleto, datediff(to_date,from_date) AS Dias
+
+FROM employees e
+
+JOIN dept_emp de
+
+ON e.emp_no = de.emp_no
+
+JOIN departments d
+
+ON de.dept_no = d.dept_no
+
+ORDER BY datediff(to_date,from_date) DESC
+
+LIMIT 10
+
